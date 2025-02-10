7c554898eeb3373e446ee7a59d0f6107237865f3
FordU759 <robinsonr355@gmail.com>
2025-02-09 20:32:59 -0500

    Update ElevatorMoveMode.java

--- src/main/java/frc/robot/autonomous/modes/ElevatorMoveMode.java
+++ src/main/java/frc/robot/autonomous/modes/ElevatorMoveMode.java
@@ -1,24 +1,16 @@
 package frc.robot.autonomous.modes;
 
 import frc.robot.autonomous.tasks.ElevatorMoveTask;
-import frc.robot.Constants;
 import frc.robot.subsystems.Elevator;
 
+public class ElevatorMoveMode extends AutoModeBase {
 
-public class ElevatorMoveMode {
+    @Override
+    public void queueTasks() {
+        // Define the target elevator state
+        Elevator.ElevatorState targetState = Elevator.ElevatorState.L3; // Example: Move to L3
 
-    public void run() {
-        // Choose the target elevator state
-        Elevator.ElevatorState targetState = Elevator.ElevatorState.L3; // Example: Go to L3
-
-        // Create and run the ElevatorMoveTask
-        ElevatorMoveTask elevatorTask = new ElevatorMoveTask(0, 0, targetState); //Distance and speed are irrelevant here.
-        elevatorTask.start();
-
-        while (!elevatorTask.isFinished()) {
-            elevatorTask.update();
-        }
-        //The done() method is called automatically in the ElevatorMoveTask when finished.
-        //elevatorTask.done(); //This is not needed as it's handled in the task itself.
+        // Queue the ElevatorMoveTask (instead of manually starting it)
+        queueTask(new ElevatorMoveTask(0, 0, targetState)); // Distance and speed are irrelevant here
     }
 }
